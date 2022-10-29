81d2c6b9f221b96378af9a04a2886a98ebae8288
Luoshan Cai <60723329+cailuoshan@users.noreply.github.com>
2022-10-29 14:24:06 +0800

    finish first version

--- http-server.c
+++ http-server.c
@@ -78,44 +78,31 @@ void Create_80_port(){
 }
 
 /*  
- *  GET http://10.0.0.1/dir/index.html HTTP/1.1\r\n
+ *  all http requests return 301, new https URL
+ *  GET http://10.0.0.1/index.html HTTP/1.1\r\n or notfound.html or dir/index.html
+ *  Range: bytes=100-200\r\n
+ *  ......
  */
 void Handle_HTTP_Request(int csock){
-    char msg[200];
+    char msg[256];
     int msg_len = 0;
-    // receive a message from client and parse the request
+    // receive a message from client
     while ((msg_len = recv(csock, msg, sizeof(msg), 0)) > 0) {
-        /* get the request method */
-        char method[5];
-        int i=0,j=0;
-        while((msg[j]!=' ') && (i<sizeof(method)) && (j<msg_len)){
-            method[i] = msg[j];
-            i++;j++;
+        /* parse the request */
+        char url[50];
+        int i = 0;
+        char *str_start = strstr(msg,'http://');
+        while (*str_start != ' ')
+        {
+            url[i++]=*str_start;
+            str_start++;
+            
+            if (i==4)
+                url[i++] = 's';        
         }
-        method[i] = '\0';
-        /* if not GET, claim error */
-    	if (strcmp(method, "GET"))
-    	{
-        	perror("request failed!");
-        	fflush(stdout);
-        	return;
-    	}
-        /* get the url addr and change to https url*/
-    	char url[100];
-        i = 0;
-    	j+=2;
-    	while ((msg[j]!=' ') && (i<sizeof(url)) && (j<msg_len))
-    	{
-        	url[i] = msg[j];
-            i++; j++;
-
-            if (i==3){
-                url[i++] = 's';
-            }
-    	}
-    	url[i] = '\0';
+        url[i] = '\0';
         
-    	/* ******************************* 
+        /* ******************************* 
          * respond with 301 new location:  
          * HTTP/1.1 301 Moved Permanently
          * Location: https://10.0.0.1/index.html
@@ -235,30 +222,62 @@ SSL_CTX *InitSSL(){
     return ctx;
 }
 
+/*  
+ *  GET https://10.0.0.1/index.html HTTP/1.1\r\n or notfound.html or dir/index.html
+ *  Range: bytes=100-200\r\n
+ *  ......
+ */
 void Handle_HTTPS_Request(void *args){
     SSL *ssl = (*(param *)args).ssl;
     int csock = (*(param *)args).csock;
-    char msg[200];
+    char msg[256];
     int msg_len = 0;
     // receive a message from client and parse the request
     while ((msg_len = SSL_read(ssl, msg, sizeof(msg))) > 0) {
-        /* TODO: parse the coming message detailed !!!!!!! path\range*/
-        char file_path[100] = "/../index.html";
-        struct stat file_stat;
-    	if (stat(file_path, &file_stat) == -1) {
-        	/*no such file*/
-        	char buf[1024];
-            strcpy(buf, "HTTP/1.1 404 File Not Found\r\n");
-    		strcat(buf,"Server: 10.0.0.1\r\n");
-            strcat(buf,"Connection: close\r\n");
-    		strcat(buf,"\r\n");
-    		SSL_write(ssl, buf, strlen(buf));
-    	}else if(){
-            /* TODO: read the partial file */
-      		
-    	}else {
-            /* TODO: read the total file */
+        /* parse the request */
+        char *str_start = NULL;
+        char file_path[20];
+        int range_start = -1;
+        int range_end = -1;
+        if (str_start = strstr(msg,'10.0.0.1'))
+        {
+            str_start += 9;
+            int i=0;
+            while (*str_start != ' '){
+                file_path[i++] = *str_start;
+                str_start++;
+            }
+            file_path[i]='\0';
+        }else{
+            perror("parse request failed!");
+        	fflush(stdout);
+        	return;
         }
+
+        if (str_start = strstr(msg,'Range'))
+        {
+            str_start += 12;
+            char tmp[10];
+            int i=0;
+            while (*str_start != '-'){
+                tmp[i++]=*str_start;
+                str_start++;     
+            }
+            tmp[i]='\0';
+            range_start=atoi(tmp);
+            i=0;str_start++;
+            while (*str_start != '\r'){
+                tmp[i++]=*str_start;
+                str_start++;     
+            }
+            if (i!=0){
+                tmp[i]='\0';
+                range_end=atoi(tmp);
+            }
+        }
+        
+        /* look up the file and make response */
+        https_serve_response(ssl, file_path, range_start, range_end);
 
         // ssl shutdown
         SSL_shutdown(ssl);
@@ -274,18 +293,41 @@ void Handle_HTTPS_Request(void *args){
     return;    
 }
 
-void serve_file(SSL *ssl, const char *filename, struct stat st)
-{
-    FILE *resource = NULL;
+void https_serve_response(SSL *ssl, const char *filename, int range_start, int range_end)
+{   
+    FILE *file = NULL;
+    struct stat file_stat;
     char buf[1024];
-    
-    /*open the file*/
-    if((resource = fopen(filename, "r"))==NULL){
-        perror("open failed");
-		return;
+	if (stat(filename, &file_stat) == -1) {
+        /*no such file*/
+        strcpy(buf, "HTTP/1.1 404 File Not Found\r\n");
+    	strcat(buf,"Server: 10.0.0.1\r\n");
+        strcat(buf,"Connection: close\r\n");
+    	strcat(buf,"\r\n");
+    	SSL_write(ssl, buf, strlen(buf));
+    }else {
+        if((file = fopen(filename, "r"))==NULL){
+            perror("open failed");
+		    return;
+        }
+        /*send HTTP header */
+        strcpy(buf, "HTTP/1.1 200 OK\r\n");
+        SSL_write(ssl, buf, strlen(buf));
+        sprintf(buf, "Content-Type: %s\r\n",filename);
+        SSL_write(ssl, buf, strlen(buf));
+        sprintf(buf, "Content-Length: %ld\r\n",st.st_size);  // need change!!
+        SSL_write(ssl, buf, strlen(buf));
+        strcpy(buf,"Server: 10.0.0.1\r\n");
+        strcat(buf,"\r\n");
+ 	    SSL_write(ssl, buf, strlen(buf));
+        /*send file body*/
+        cat(ssl, file, range_start, range_end);
+        fclose(file);
     }
 
-        /*void *html_ = mmap(nullptr, file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    /*open the file*/
+
+        /*
         string buf_w = "HTTP/1.1 200 OK\r\n"
                         "Content-Type: text/html; charset=UTF-8\r\n"
                         "Connection: close\r\n"
@@ -295,34 +337,43 @@ void serve_file(SSL *ssl, const char *filename, struct stat st)
         buf_w += (char *)html_;
         SSL_write(ssl, (void*)buf_w.c_str(), buf_w.size());
         munmap(html_, file_stat.st_size);*/
-
-    /*send HTTP header */
-    strcpy(buf, "HTTP/1.1 200 OK\r\n");
-    SSL_write(ssl, buf, strlen(buf));
-    sprintf(buf, "Content-Type: %s\r\n",filename);
-    SSL_write(ssl, buf, strlen(buf));
-    sprintf(buf, "Content-Length: %ld\r\n",st.st_size);
-    SSL_write(ssl, buf, strlen(buf));
-    strcpy(buf,"Server: 10.0.0.1\r\n");
-    strcat(buf,"\r\n");
- 	SSL_write(ssl, buf, strlen(buf));
-    /*send file body*/
-    cat(ssl, resource);
-    fclose(resource);
+    
 }
 /**********************************************************************/
 /* Put the entire contents of a file out on a socket.  
  * Parameters: the client socket descriptor
  *             FILE pointer for the file to cat */
 /**********************************************************************/
-void cat(SSL *ssl, FILE *resource)
+void cat(SSL *ssl, FILE *resource, int range_start, int range_end)
 {
     char buf[1024];
     int size;
-    size = fread(buf,1,1024,resource);
-    send(csock, buf, size, 0);
-    while(!feof(resource)){
-        size = fread(buf,1,1024,resource);
-        send(csock, buf, size, 0);
+    if (range_start == -1){
+        while(!feof(resource)){
+            size = fread(buf,1,1024,resource);
+            SSL_write(ssl, buf, size);
+        }
+    } else if (range_start!=-1 && range_end==-1){
+        fseek(resource, range_start, SEEK_SET);
+        while(!feof(resource)){
+            size = fread(buf,1,1024,resource);
+            SSL_write(ssl, buf, size);
+        }
+    } else {
+        fseek(resource, range_start, SEEK_SET);
+        int total_size = 0;
+        while (total_size < (range_end-range_start) && !feof(resource))
+        {
+            int remain_size = range_end-range_start-total_size;
+            if (remain_size <= 1024)
+            {
+                size = fread(buf,1,remain_size,resource);
+            }else{
+                size = fread(buf,1,1024,resource);
+            }
+            total_size += size;
+            SSL_write(ssl, buf, size);
+        }
     }
+    
 }
